e4b1096c4f09e4769e263249353359b4b864a8bf
Boris Vlasevsky <fallTroy@protonmail.com>
2024-08-25 22:27:02 +0400

    Added messaging implementation

---
+++ broker-server/src/test/java/com/example/brokerserver/BrokerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brokerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
