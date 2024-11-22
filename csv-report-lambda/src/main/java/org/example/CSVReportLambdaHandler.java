1d594753e1507aa25c513ad77e1962c8aca386b2
Boris Vlasevsky <fallTroy@protonmail.com>
2024-11-22 13:01:19 +0400

    refactoring lambda function

--- csv-report-lambda/src/main/java/org/example/CSVReportLambdaHandler.java
+++ csv-report-lambda/src/main/java/org/example/CSVReportLambdaHandler.java
@@ -3,17 +3,17 @@ package org.example;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.SdkClientException;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.*;
 import com.amazonaws.services.dynamodbv2.document.spec.ScanSpec;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -21,9 +21,8 @@ import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class CSVReportLambdaHandler implements RequestHandler<Object, String> {
 
@@ -31,125 +30,127 @@ public class CSVReportLambdaHandler implements RequestHandler<Object, String> {
     private static final String BUCKET_NAME = "borisvlasevsky-webapp-bucket";
     private static final String REGION = "us-east-1";
 
+    private static final AmazonDynamoDB dynamoDBClient = AmazonDynamoDBClientBuilder.standard()
+            .withRegion(REGION)
+            .build();
+
+    private static final DynamoDB dynamoDB = new DynamoDB(dynamoDBClient);
+
+    private static final AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+            .withRegion(REGION)
+            .build();
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public String handleRequest(Object input, Context context) {
         context.getLogger().log("Starting CSVReportLambdaHandler...");
 
-        AmazonDynamoDB dynamoDBClient = null;
-        AmazonS3 s3Client = null;
         try {
-            dynamoDBClient = AmazonDynamoDBClientBuilder.standard()
-                    .withRegion(REGION)
-                    .build();
+            List<TrainerData> trainerDataList = fetchTrainerData(context);
 
-            DynamoDB dynamoDB = new DynamoDB(dynamoDBClient);
-            ObjectMapper objectMapper = new ObjectMapper();
-
-            Table table = dynamoDB.getTable(TABLE_NAME);
-
-            ScanSpec scanSpec = new ScanSpec();
-            ItemCollection<ScanOutcome> items = table.scan(scanSpec);
-
-            // Prepare data for CSV
-            List<TrainerData> trainerDataList = new ArrayList<>();
-
-            LocalDate now = LocalDate.now(ZoneId.of("UTC"));
-            int currentYear = now.getYear();
-            int currentMonth = now.getMonthValue();
-
-            for (Item item : items) {
-                String firstName = item.getString("firstName");
-                String lastName = item.getString("lastName");
-                boolean isActive = item.getBoolean("isActive");
-                String yearlySummariesJson = item.getString("yearlySummaries");
-
-                try {
-                    YearlyTrainingSummary[] yearlySummaries = objectMapper.readValue(yearlySummariesJson, YearlyTrainingSummary[].class);
-
-                    int currentMonthDuration = 0;
-                    for (YearlyTrainingSummary yts : yearlySummaries) {
-                        if (yts.getTrainingYear() == currentYear) {
-                            Integer monthDuration = yts.getMonthlySummary().get(currentMonth);
-                            if (monthDuration != null) {
-                                currentMonthDuration = monthDuration;
-                                break;
-                            }
-                        }
-                    }
-
-                    if (!isActive && currentMonthDuration == 0) {
-                        continue;
-                    }
-
-                    trainerDataList.add(new TrainerData(firstName, lastName, currentMonthDuration));
-
-                } catch (Exception e) {
-                    context.getLogger().log("Error parsing yearlySummaries for trainer " + firstName + " " + lastName + ": " + e.getMessage());
-                }
+            if (trainerDataList.isEmpty()) {
+                context.getLogger().log("No trainer data to process.");
+                return "Success";
             }
 
-            // Generate CSV
-            try {
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                OutputStreamWriter osw = new OutputStreamWriter(baos, StandardCharsets.UTF_8);
-                CSVPrinter csvPrinter = new CSVPrinter(osw, CSVFormat.DEFAULT
-                        .withHeader("Trainer First Name", "Trainer Last Name", "Current Month Training Duration"));
+            ByteArrayOutputStream csvData = generateCSVReport(trainerDataList, context);
 
-                for (TrainerData trainerData : trainerDataList) {
-                    csvPrinter.printRecord(trainerData.getFirstName(), trainerData.getLastName(), trainerData.getCurrentMonthDuration());
-                }
+            uploadToS3(csvData, context);
 
-                csvPrinter.flush();
+            return "Success";
 
-                // Initialize S3 client
-                s3Client = AmazonS3ClientBuilder.standard()
-                        .withRegion(REGION)
-                        .build();
-
-                String reportName = String.format("Trainers_Trainings_summary_%d_%02d.csv", currentYear, currentMonth);
-
-                ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
-
-                // Set metadata with content length
-                ObjectMetadata metadata = new ObjectMetadata();
-                metadata.setContentLength(baos.size());
-                metadata.setContentType("text/csv");
-
-                // Upload to S3
-                s3Client.putObject(BUCKET_NAME, reportName, bais, metadata);
-
-                context.getLogger().log("CSV report uploaded successfully to S3 bucket: " + BUCKET_NAME + ", with key: " + reportName);
-
-            } catch (AmazonServiceException e) {
-                context.getLogger().log("AmazonServiceException: " + e.getMessage());
-                return "Error";
-            } catch (SdkClientException e) {
-                context.getLogger().log("SdkClientException: " + e.getMessage());
-                return "Error";
-            } catch (Exception e) {
-                context.getLogger().log("Exception: " + e.getMessage());
-                return "Error";
-            }
-
-        } catch (AmazonServiceException e) {
-            context.getLogger().log("AmazonServiceException: " + e.getMessage());
-            return "Error";
-        } catch (SdkClientException e) {
-            context.getLogger().log("SdkClientException: " + e.getMessage());
-            return "Error";
         } catch (Exception e) {
             context.getLogger().log("Exception: " + e.getMessage());
             return "Error";
         }
-
-        return "Success";
     }
 
-    // Helper class to hold trainer data
+    private List<TrainerData> fetchTrainerData(Context context) throws Exception {
+        Table table = dynamoDB.getTable(TABLE_NAME);
+        ItemCollection<ScanOutcome> items = table.scan(new ScanSpec());
+
+        LocalDate now = LocalDate.now(ZoneId.of("UTC"));
+        int currentYear = now.getYear();
+        int currentMonth = now.getMonthValue();
+
+        List<Item> itemsList = new ArrayList<>();
+        items.forEach(itemsList::add);
+
+        return itemsList.stream()
+                .map(item -> parseTrainerData(item, currentYear, currentMonth, context))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
+
+    private TrainerData parseTrainerData(Item item, int currentYear, int currentMonth, Context context) {
+        String firstName = item.getString("firstName");
+        String lastName = item.getString("lastName");
+        boolean isActive = item.getBoolean("isActive");
+        String yearlySummariesJson = item.getString("yearlySummaries");
+
+        try {
+            YearlyTrainingSummary[] yearlySummaries = objectMapper.readValue(yearlySummariesJson, YearlyTrainingSummary[].class);
+
+            int currentMonthDuration = Arrays.stream(yearlySummaries)
+                    .filter(yts -> yts.getTrainingYear() == currentYear)
+                    .map(yts -> yts.getMonthlySummary().getOrDefault(currentMonth, 0))
+                    .findFirst()
+                    .orElse(0);
+
+            if (!isActive && currentMonthDuration == 0) {
+                context.getLogger().log("Skip the trainer " + firstName + " " + lastName + ": inactive and there are no trainings in the current month.");
+            }
+
+            return new TrainerData(firstName, lastName, currentMonthDuration);
+
+        } catch (Exception e) {
+            context.getLogger().log("Error parsing yearlySummaries for trainer " + firstName + " " + lastName + ": " + e.getMessage());
+            return null;
+        }
+    }
+
+    private ByteArrayOutputStream generateCSVReport(List<TrainerData> trainerDataList, Context context) throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (OutputStreamWriter osw = new OutputStreamWriter(baos, StandardCharsets.UTF_8);
+             CSVPrinter csvPrinter = new CSVPrinter(osw, CSVFormat.DEFAULT
+                     .withHeader("Trainer First Name", "Trainer Last Name", "Current Month Training Duration"))) {
+
+            trainerDataList.forEach(trainerData -> {
+                try {
+                    csvPrinter.printRecord(trainerData.getFirstName(), trainerData.getLastName(), trainerData.getCurrentMonthDuration());
+                } catch (Exception e) {
+                    context.getLogger().log("Ошибка при записи данных тренера " + trainerData.getFirstName() + " " + trainerData.getLastName() + ": " + e.getMessage());
+                }
+            });
+
+            csvPrinter.flush();
+        }
+        return baos;
+    }
+
+    private void uploadToS3(ByteArrayOutputStream csvData, Context context) throws AmazonServiceException, SdkClientException {
+        LocalDate now = LocalDate.now(ZoneId.of("UTC"));
+        int currentYear = now.getYear();
+        int currentMonth = now.getMonthValue();
+
+        String reportName = String.format("Trainers_Trainings_summary_%d_%02d.csv", currentYear, currentMonth);
+
+        ByteArrayInputStream bais = new ByteArrayInputStream(csvData.toByteArray());
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(csvData.size());
+        metadata.setContentType("text/csv");
+
+        s3Client.putObject(BUCKET_NAME, reportName, bais, metadata);
+
+        context.getLogger().log("CSV report uploaded successfully to S3 bucket: " + BUCKET_NAME + ", with key: " + reportName);
+    }
+
     static class TrainerData {
-        private String firstName;
-        private String lastName;
-        private int currentMonthDuration;
+        private final String firstName;
+        private final String lastName;
+        private final int currentMonthDuration;
 
         public TrainerData(String firstName, String lastName, int currentMonthDuration) {
             this.firstName = firstName;
@@ -170,7 +171,6 @@ public class CSVReportLambdaHandler implements RequestHandler<Object, String> {
         }
     }
 
-    // YearlyTrainingSummary class
     static class YearlyTrainingSummary {
         private int trainingYear;
         private Map<Integer, Integer> monthlySummary;
